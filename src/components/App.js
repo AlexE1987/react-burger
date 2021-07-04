69903939951cebf9431a18cc152a1d819eaea5b9
AlexE1987 <evsikovad1987@gmail.com>
2021-07-04 22:46:07 +0400

    feat: add inputs and buttons in MenuAdmin

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,25 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import MenuAdmin from './MenuAdmin';
+import sampleBurgers from '../sample-burgers';
 
 class App extends React.Component {
+  state = {
+    burgers: {},
+    order: {},
+  };
+
+  addBurger = (burger) => {
+    const burgers = { ...this.state.burger };
+    burgers[`burgers${Date.now()}`] = burger;
+    this.setState({ burgers });
+  };
+
+  loadSampleBurgers = () => {
+    this.setState({ burgers: sampleBurgers });
+    console.log('Load');
+  };
+
   render() {
     return (
       <div className="burger-paradise">
@@ -11,7 +28,7 @@ class App extends React.Component {
           <Header title="Very Hot Burger" amount={10} hot={true} />
         </div>
         <Order />
-        <MenuAdmin />
+        <MenuAdmin addBurger={this.addBurger} loadSampleBurgers={this.loadSampleBurgers} />
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,7 +19,8 @@ class Landing extends React.Component {
   };
 
   goToRestaurant = () => {
-    console.log('go to rest');
+    const { url } = this.state;
+    this.props.history.push(`/restaurant/${url}`);
   };
 
   render() {
